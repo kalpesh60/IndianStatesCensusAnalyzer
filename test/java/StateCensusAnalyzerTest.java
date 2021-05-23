1515190ed9f968bd512a89b9b2add338fba46103
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-23 23:40:41 +0530

    Added TestCode if file is incorrect throw Custom Exception

--- test/java/StateCensusAnalyzerTest.java
+++ test/java/StateCensusAnalyzerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class StateCensusAnalyzerTest {
 
     private static final String INDIA_STATE_CSV_FILEPATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INCORRECT_FILE = "./src";
 
     @Test
     public void givenIndiaCensusCSVFileShouldReturnCorrectRecords() throws CensusAnalyzerException {
@@ -11,5 +12,16 @@ public class StateCensusAnalyzerTest {
         int numOfRecords = stateCensusAnalyzer.loadIndiaCensusData(INDIA_STATE_CSV_FILEPATH);
         System.out.println(numOfRecords);
         Assertions.assertEquals(29,numOfRecords);
+    }
+
+    @Test
+    public void givenStateCensusCSVFileIsIncorrectShouldThrowCustomException() {
+        StateCensusAnalyzer stateCensusAnalyzer = new StateCensusAnalyzer();
+        try {
+            stateCensusAnalyzer.loadIndiaCensusData(INCORRECT_FILE);
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyzerException.CensusExceptionType.NO_SUCH_FILE, e.type);
+        }
     }
 }
