d48539cc6d9724fe864ee2b975821709270e1328
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-26 14:19:06 +0530

    Added Code to load State Code info from CSV File

--- main/java/StateCensusAnalyzer.java
+++ main/java/StateCensusAnalyzer.java
@@ -23,7 +23,29 @@ public class StateCensusAnalyzer {
                 numOfEntries++;
                 IndiaCensusCSV censusData = censusCSVIterator.next();
             }
+        } catch (NoSuchFileException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.CensusExceptionType.NO_SUCH_FILE, e.getMessage());
+        } catch (RuntimeException e) {
+            throw new CensusAnalyzerException(CensusAnalyzerException.CensusExceptionType.WRONG_DELEMETER_HEADER, "incorrect delimeter");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numOfEntries;
+    }
 
+    public int loadIndiaStateCodeData(String CsvFilepath) throws CensusAnalyzerException {
+        int numOfEntries = 0;
+        try (Reader reader = Files.newBufferedReader(Paths.get(CsvFilepath))) {
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<IndiaStateCodeCSV>(reader);
+            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+            numOfEntries = 0;
+            while (censusCSVIterator.hasNext()) {
+                numOfEntries++;
+                IndiaStateCodeCSV censusData = censusCSVIterator.next();
+            }
         } catch (NoSuchFileException e) {
             throw new CensusAnalyzerException(CensusAnalyzerException.CensusExceptionType.NO_SUCH_FILE, e.getMessage());
         } catch (RuntimeException e) {
